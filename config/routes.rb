dc5902c9f204ddacbd2a162987c953b887c803fd
azufb <curiousazunas@gmail.com>
2023-03-23 22:23:40 +0900

    ルーティング作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  # アプリケーションのrootパスをArticlesControllerとアクションに対応づける
+  root "articles#index"
+  
+  # GET /articlesリクエストをArticlesControllerのindexアクションに対応づける
+  get "/articles", to: "articles#index"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
