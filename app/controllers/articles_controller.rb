e680cff283aac00396503f76b3904ec5d02c0fd5
azufb <curiousazunas@gmail.com>
2023-03-25 18:04:23 +0900

    controller更新

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,11 @@
+# コントローラは、アプリケーションに対する特定のリクエストを受け取って処理する
+# アクションは、リクエストを扱うために必要な処理を実行する(インスタンスメソッド)
+
 class ArticlesController < ApplicationController
+  # indexアクション
   def index
+    # データベースから全ての記事を取り出す
+    # @articlesはインスタンス変数で、ビューからも参照可能。
+    @articles = Article.all
   end
 end
