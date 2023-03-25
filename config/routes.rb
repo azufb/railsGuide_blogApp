c0a5456d97122644702ff76fa7ceb2cdd81d73eb
azufb <curiousazunas@gmail.com>
2023-03-25 18:30:41 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
   # アプリケーションのrootパスをArticlesControllerとアクションに対応づける
   root "articles#index"
-  
+
+  # resourcesを使うことで、リソースの集まりを対応づけるのによく使われるルーティングを全て対応づけてくれる
+  # いちいち、get パス, to: コントローラ#メソッド名と書かなくて良い
+  resources :articles
+
   # GET /articlesリクエストをArticlesControllerのindexアクションに対応づける
-  get "/articles", to: "articles#index"
+  # get "/articles", to: "articles#index"
+
+  # 1件の記事のタイトルと本文を表示したい
+  # GET /articles/:idをArticlesControllerのshowアクションに対応づける
+  # :idは、ルーティングのパラメータで、その値をparamsと言うハッシュに保存する
+  # get "/articles/:id", to: "articles#show"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
