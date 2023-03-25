0633f0aeaa665877902dd685be6a9b67a5f6af57
azufb <curiousazunas@gmail.com>
2023-03-25 19:42:51 +0900

    バリデーション作成

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,10 @@
 # Active Recordと言うRailsの機能を介してアプリケーションのデータベースとやり取りする
 
 class Article < ApplicationRecord
+    # validatesでバリデーション
+
+    # titleは必須入力
+    validates :title, presence: true
+    # bodyは必須入力で10文字以上
+    validates :body, presence: true, length: { minimum: 10 }
 end
