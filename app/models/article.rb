83d7f4867fabb42156d4e63ed432e058efb46d4d
azufb <curiousazunas@gmail.com>
2023-03-26 13:46:14 +0900

    記事とコメントの関連付け

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,8 +2,10 @@
 # Active Recordと言うRailsの機能を介してアプリケーションのデータベースとやり取りする
 
 class Article < ApplicationRecord
-    # validatesでバリデーション
+    # has_manyは、1件の記事は複数のコメントを持つことを示す(1対多)
+    has_many :comments
 
+    # validatesでバリデーション
     # titleは必須入力
     validates :title, presence: true
     # bodyは必須入力で10文字以上
